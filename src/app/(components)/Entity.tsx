2b6e9e28b737e3879f45b2f4a6e80fc92cce1b07
ynawhocodes <ynawhocodes@gmail.com>
2024-03-09 03:32:09 +0900

    [feat] Entity 컴포넌트 생성

---
+++ src/app/(components)/Entity.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+const Entity = () => {
+  return (
+    <div className="bg-white p-3 rounded-lg">
+      <h3 className="font-semibold px-2 py-3 text-lg">Entity</h3>
+      <div className="flex flex-col">
+        <Link
+          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          href=""
+        >
+          User
+        </Link>
+        <Link
+          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          href=""
+        >
+          Post
+        </Link>
+        <Link
+          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          href=""
+        >
+          Comment
+        </Link>
+        <Link
+          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          href=""
+        >
+          Address
+        </Link>
+        <Link
+          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          href=""
+        >
+          Image
+        </Link>
+        <Link
+          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          href=""
+        >
+          Video
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Entity;
