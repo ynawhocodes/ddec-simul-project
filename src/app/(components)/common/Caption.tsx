2d9b28abfc1fc0e399d474f69299a225e376ffb2
ynawhocodes <ynawhocodes@gmail.com>
2024-03-10 23:51:20 +0900

    [feat] Caption 컴포넌트 생성

---
+++ src/app/(components)/common/Caption.tsx
@@ -0,0 +1,122 @@
+import { captionInfo } from "@/_assets/data/caption";
+import { getStorageType } from "@/_utils/getStorageType";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const Caption = ({
+  level,
+  localId,
+  handleStorageTypeByLocalId,
+}: {
+  level: number;
+  localId: number;
+  handleStorageTypeByLocalId: (localId: number, storageType: string) => void;
+}) => {
+  const [storageType, setStorageType] = useState("CLOUD");
+
+  return (
+    <div className="p-3 flex flex-col gap-1 absolute left-10 top-10 bg-white/80 rounded-lg shadow-lg flex-nowrap cursor-pointer">
+      <div className="flex gap-2 border-b pb-2 border-gray-300">
+        <p className="font-semibold whitespace-nowrap text-[14px]">
+          Storage type
+        </p>
+        <div className="flex gap-2 items-center">
+          <div className="flex items-center gap-1">
+            <input
+              className="checked:bg-slate-500"
+              type="radio"
+              value={storageType}
+              checked={getStorageType(level, localId) === "CLOUD"}
+              onChange={() => {
+                setStorageType("CLOUD");
+                handleStorageTypeByLocalId(localId, "CLOUD");
+              }}
+            />
+            <label className="text-[12px]">Cloud</label>
+          </div>
+          <div className="flex items-center gap-1">
+            <input
+              className="checked:bg-slate-500"
+              type="radio"
+              value={storageType}
+              checked={getStorageType(level, localId) === "EDGE"}
+              onChange={() => {
+                setStorageType("EDGE");
+                handleStorageTypeByLocalId(localId, "EDGE");
+              }}
+            />
+            <label className="text-[12px]">Edge</label>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-1">
+        <p className="font-semibold whitespace-nowrap text-[12px]">
+          Location id:
+        </p>
+        <p className="text-[12px]">{`${captionInfo[localId - 1].local_id}`}</p>
+      </div>
+      <div className="flex gap-1">
+        <p className="font-semibold whitespace-nowrap text-[12px]">
+          Storage id:
+        </p>
+        <p className="text-[12px]">
+          {getStorageType(level, localId) === "CLOUD"
+            ? "0"
+            : captionInfo[localId - 1].local_id}
+        </p>
+      </div>
+      <div className="flex gap-1">
+        <p className="font-semibold whitespace-nowrap text-[12px]">
+          Data size:
+        </p>
+        <p className="text-[12px]">{`${
+          captionInfo[localId - 1].data_size
+        } GB`}</p>
+      </div>
+      <div className="flex gap-1">
+        <p className="font-semibold whitespace-nowrap text-[12px]">
+          # of users:
+        </p>
+        <p className="text-[12px]">{`${
+          captionInfo[localId - 1].population
+        }`}</p>
+      </div>
+    </div>
+  );
+};
+
+const CaptionProvider = ({
+  level,
+  localId,
+  children,
+  handleStorageTypeByLocalId,
+}: {
+  level: number;
+  localId: number;
+  handleStorageTypeByLocalId: (localId: number, storageType: string) => void;
+  children: React.ReactNode;
+}) => {
+  const [isHover, setIsHover] = useState(false);
+  return (
+    <motion.div
+      className="relative"
+      onHoverStart={() => {
+        setIsHover(true);
+      }}
+      onHoverEnd={() => setIsHover(false)}
+      whileHover={{ scale: 1.2, zIndex: 10 }}
+    >
+      {children}
+      {isHover && (
+        <Caption
+          level={level}
+          localId={localId}
+          handleStorageTypeByLocalId={handleStorageTypeByLocalId}
+        />
+      )}
+    </motion.div>
+  );
+};
+
+export default CaptionProvider;
+export { Caption };
