4536bb7512fa2c34fc13348002f0d2d581f5ce48
ynawhocodes <ynawhocodes@gmail.com>
2024-03-10 23:48:56 +0900

    [feat] 블럭 svg props에 storageType 추가

---
+++ src/_assets/icons.tsx
@@ -0,0 +1,271 @@
+const OneBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="243"
+      // height="142"
+      viewBox="0 0 243 142"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.0605469"
+        y="1.80908"
+        width="241.609"
+        height="139.489"
+        transform="rotate(-0.222619 0.0605469 1.80908)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const TwoBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="236"
+      // height="129"
+      viewBox="0 0 236 129"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.669922"
+        y="0.0776367"
+        width="234.791"
+        height="127.791"
+        transform="rotate(0.0912114 0.669922 0.0776367)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const ThreeBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="165"
+      // height="243"
+      viewBox="0 0 165 243"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.42285"
+        y="0.714844"
+        width="163.043"
+        height="241.235"
+        transform="rotate(0.206747 1.42285 0.714844)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const FourBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="236"
+      // height="191"
+      viewBox="0 0 236 191"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.92383"
+        y="0.977539"
+        width="234.072"
+        height="188.193"
+        transform="rotate(0.30853 1.92383 0.977539)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const FiveBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="361"
+      // height="239"
+      viewBox="0 0 361 239"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 187V0L360.5 1.5V239H136V187H0Z"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const SixBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="226"
+      // height="131"
+      viewBox="0 0 226 131"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.37305"
+        y="0.681641"
+        width="224.538"
+        height="128.553"
+        transform="rotate(0.206747 1.37305 0.681641)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const SevenBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="246"
+      // height="136"
+      viewBox="0 0 246 136"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.776367"
+        y="0.834961"
+        width="244.567"
+        height="133.875"
+        transform="rotate(0.206747 0.776367 0.834961)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const EightBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="235"
+      // height="238"
+      viewBox="0 0 235 238"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.16406"
+        y="0.444336"
+        width="233.712"
+        height="236.714"
+        transform="rotate(0.206747 1.16406 0.444336)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const NineBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="265"
+      // height="183"
+      viewBox="0 0 265 183"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M167.5 59.5V0H264.5V182.5H0.5V59.5H167.5Z"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const TenBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="239"
+      // height="182"
+      viewBox="0 0 239 182"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.30371"
+        y="0.000976562"
+        width="237.62"
+        height="180.641"
+        transform="rotate(0.206747 1.30371 0.000976562)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const ElevenBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="233"
+      // height="142"
+      viewBox="0 0 233 142"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.02148"
+        y="-0.00537109"
+        width="231.326"
+        height="140.251"
+        transform="rotate(0.206747 1.02148 -0.00537109)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+const TwelveBlock = ({ storageType }: { storageType: string }) => {
+  return (
+    <svg
+      className="w-full h-full"
+      // width="254"
+      // height="133"
+      viewBox="0 0 254 133"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.39551"
+        y="0.171875"
+        width="252.231"
+        height="131.272"
+        transform="rotate(0.206747 1.39551 0.171875)"
+        fill="#5D63F1"
+        fillOpacity={storageType === "EDGE" ? "0.75" : "0.25"}
+      />
+    </svg>
+  );
+};
+
+export {
+  OneBlock,
+  TwoBlock,
+  ThreeBlock,
+  FourBlock,
+  FiveBlock,
+  SixBlock,
+  SevenBlock,
+  EightBlock,
+  NineBlock,
+  TenBlock,
+  ElevenBlock,
+  TwelveBlock,
+};
