2474d08e88b58627f1dc73e5b0f258b16f10900d
yanwhocodes <ynawhocodes@gmail.com>
2024-03-14 00:43:22 +0900

    [feat] defaultResultValue 추가

---
+++ src/_assets/data/defaultResultValue.ts
@@ -0,0 +1,17 @@
+const defaultResultValue = {
+  "00cost": 0,
+  "01avg_response_time": 0,
+  "02avg_response_time_in_edge": 0,
+  "03avg_response_time_in_cloud": 0,
+  "04min_response_time": 0,
+  "05max_response_time": 0,
+  "06success_rate_in_downtime($s)": 0,
+  "07success_rate_in_downtime($s) - edge": 0,
+  "08success_rate_in_downtime($s) - cloud": 0,
+  "09range_of_benefit(edge)": 0,
+  "10latency_difference": 0,
+  "11total_cost": 0,
+  "12total_improvement": 0,
+};
+
+export { defaultResultValue };
