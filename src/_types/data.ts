bc9c31f3e54817ea9b6d11cc717ee2ecb35ac529
yanwhocodes <ynawhocodes@gmail.com>
2024-03-14 00:44:08 +0900

    [feat] ResultType 생성

--- src/_types/data.ts
+++ src/_types/data.ts
@@ -3,3 +3,25 @@ export interface StrategyType {
   storage_type: string;
   storage_id: number;
 }
+export interface ResultType {
+  //  Monthly Cost for the distribution
+  "00cost": number;
+  // 1) Average response time
+  "01avg_response_time": number;
+  "02avg_response_time_in_edge": number;
+  "03avg_response_time_in_cloud": number;
+  // 3) Response time (min, max)
+  "04min_response_time": number;
+  "05max_response_time": number;
+  // 2) Success rate
+  "06success_rate_in_downtime($s)": number;
+  "07success_rate_in_downtime($s) - edge": number;
+  "08success_rate_in_downtime($s) - cloud": number;
+  // 4) Range of benefit
+  "09range_of_benefit(edge)": number;
+  // 5) Latency difference
+  "10latency_difference": number;
+  // Total Result
+  "11total_cost": number;
+  "12total_improvement": number;
+}
