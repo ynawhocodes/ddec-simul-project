136281a79a218c106af776680f0a791113edc288
yanwhocodes <ynawhocodes@gmail.com>
2024-03-14 04:28:08 +0900

    [mod] Entity UI 변경

--- src/app/(components)/Entity.tsx
+++ src/app/(components)/Entity.tsx
@@ -1,45 +1,55 @@
 import Link from "next/link";
 
-const Entity = () => {
+const Entity = ({ menu }: { menu?: string }) => {
   return (
     <div className="bg-white p-3 rounded-lg">
       <h3 className="font-semibold px-2 py-3 text-lg">Entity</h3>
       <div className="flex flex-col">
         <Link
-          className="bg-[#eee] font-semibold px-2 rounded-md py-2 text-[14px]"
-          href=""
+          className={`${
+            menu === "user" && "bg-[#eee] font-semibold"
+          } px-2 rounded-md py-2 text-[14px] hover:bg-[#eee]`}
+          href="/"
         >
           User
         </Link>
         <Link
-          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          className="hover:bg-[#eee] px-2 hover:rounded-md py-2 text-[14px]"
           href=""
         >
           Post
         </Link>
         <Link
-          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          className="hover:bg-[#eee] px-2 hover:rounded-md py-2 text-[14px]"
           href=""
         >
           Comment
         </Link>
         <Link
-          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          className="hover:bg-[#eee] px-2 hover:rounded-md py-2 text-[14px]"
           href=""
         >
           Address
         </Link>
         <Link
-          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          className="hover:bg-[#eee] px-2 hover:rounded-md py-2 text-[14px]"
           href=""
         >
           Image
         </Link>
         <Link
-          className="hover:bg-[#eee] hover:font-semibold px-2 hover:rounded-md py-2 text-[14px]"
+          className="hover:bg-[#eee] px-2 hover:rounded-md py-2 text-[14px]"
           href=""
         >
           Video
+        </Link>
+        <Link
+          className={`${
+            menu === "baseline" && "bg-[#eee] font-semibold"
+          } px-2 rounded-md py-2 text-[14px] hover:bg-[#eee]`}
+          href="/baseline"
+        >
+          Baseline
         </Link>
       </div>
     </div>
