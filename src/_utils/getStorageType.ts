6c8eff3b8b7b7b5de151925600cfe7a42b374fdb
yanwhocodes <ynawhocodes@gmail.com>
2024-03-13 19:00:41 +0900

    [mod] getStorageType util 제거

--- src/_utils/getStorageType.ts
+++
@@ -1,7 +0,0 @@
-import { strategyData } from "@/_assets/data/strategy";
-
-const getStorageType = (level: number, localId: number) => {
-  return strategyData[level]?.includes(localId) ? "EDGE" : "CLOUD";
-};
-
-export { getStorageType };
